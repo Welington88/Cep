89304286b2e94b23efed6e27003d0b3e6bd679aa
Welington Marquezini Valverde da Silva <welington_marquezini88@live.com>
2023-02-05 09:58:00 -0300

    Projeto Cep

---
+++ Cep/Program.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+Console.WriteLine("Digite um cep: ");
+string? cep = Console.ReadLine();
+string? url = $"https://viacep.com.br/ws/{cep}/json/";
+//string? url = $"https://viacep.com.br/ws/{cep}/xml/";
+
+var resultado = new HttpClient().GetStringAsync(url).Result;
+
+var cepObjeto = JsonConvert.DeserializeObject<CEP>(resultado);
+var json = JsonConvert.SerializeObject(cepObjeto, Formatting.Indented);
+
+Console.WriteLine(json);
